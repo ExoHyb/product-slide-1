5d457456fadfc03032731f285197ee6009c2e4a2
Theo Martin <theo.martin22@icloud.com>
2018-07-28 11:55:33 +0200

    [+] FO : gestion des images produits en include
    Ajout du dossier "templates" et du fichier "images-product.php"

--- index.php
+++ index.php
@@ -31,36 +31,7 @@
             </div>
         </div>
         <section class="row" id="product">
-            <div class="col-image col-xs-12 col-sm-8">
-                <div class="image-wrapper-mobile visible-xs-block">
-                    <div class="image-item">
-                        <img src="images/image-1.jpeg" alt="">
-                    </div>
-                    <div class="image-item">
-                        <img src="images/image-2.jpeg" alt="">
-                    </div>
-                    <div class="image-item">
-                        <img src="images/image-3.jpeg" alt="">
-                    </div>
-                    <div class="image-item">
-                        <img src="images/image-4.jpeg" alt="">
-                    </div>
-                </div>
-                <div class="row image-wrapper-desktop hidden-xs">
-                    <div class="image-item col-sm-6">
-                        <img src="images/image-1.jpeg" alt="">
-                    </div>
-                    <div class="image-item col-sm-6">
-                        <img src="images/image-2.jpeg" alt="">
-                    </div>
-                    <div class="image-item col-sm-6">
-                        <img src="images/image-3.jpeg" alt="">
-                    </div>
-                    <div class="image-item col-sm-6">
-                        <img src="images/image-4.jpeg" alt="">
-                    </div>
-                </div>
-            </div>
+            <?php include 'templates/images-product.php'; ?>
             <div class="col-xs-12 col-sm-4">
                 <h1>Only Play - Leggings de sport </h1>
 
---
+++ templates/images-product.php
@@ -0,0 +1,30 @@
+<div class="col-image col-xs-12 col-sm-8">
+    <div class="image-wrapper-mobile visible-xs-block">
+        <div class="image-item">
+            <img src="images/image-1.jpeg" alt="">
+        </div>
+        <div class="image-item">
+            <img src="images/image-2.jpeg" alt="">
+        </div>
+        <div class="image-item">
+            <img src="images/image-3.jpeg" alt="">
+        </div>
+        <div class="image-item">
+            <img src="images/image-4.jpeg" alt="">
+        </div>
+    </div>
+    <div class="row image-wrapper-desktop hidden-xs">
+        <div class="image-item col-sm-6">
+            <img src="images/image-1.jpeg" alt="">
+        </div>
+        <div class="image-item col-sm-6">
+            <img src="images/image-2.jpeg" alt="">
+        </div>
+        <div class="image-item col-sm-6">
+            <img src="images/image-3.jpeg" alt="">
+        </div>
+        <div class="image-item col-sm-6">
+            <img src="images/image-4.jpeg" alt="">
+        </div>
+    </div>
+</div>
