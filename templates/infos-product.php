d9d982fd628a3a97105068c3946c198a34af8b92
Theo Martin <theo.martin22@icloud.com>
2018-07-28 14:31:39 +0200

    [+] création template infos-product.php

---
+++ templates/infos-product.php
@@ -0,0 +1,9 @@
+<div class="col-xs-12 col-sm-4">
+    <h1>Only Play - Leggings de sport </h1>
+    <div class="price">
+        <p class="promo-price">23,99€</p>
+        <p class="current-price">34,99€</p>
+    </div>
+    <p id="livraison">Livraison et retour gratuits *</p>
+    <a href="#" id="link-fitguide">Guide des tailles</a>
+</div>
