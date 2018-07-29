16ac251f1fda852d79f801b72cee4a89f7fb5c8d
Theo Martin <theo.martin22@icloud.com>
2018-07-29 14:28:05 +0200

    [+] FO : ajout select taille et bouton ajout panier

--- templates/infos-product.php
+++ templates/infos-product.php
@@ -6,4 +6,18 @@
     </div>
     <p id="livraison">Livraison et retour gratuits *</p>
     <a href="#" id="link-fitguide">Guide des tailles</a>
+    <form>
+        <div class="form-group">
+            <label for="size">Taille</label>
+            <select class="form-control" id="size">
+                <option>Veuilez choisir</option>
+                <option>34</option>
+                <option>36</option>
+                <option>38</option>
+                <option>40</option>
+                <option>42</option>
+            </select>
+        </div>
+        <a href="#" class="btn btn-success">Ajouter au panier</a>
+    </form>
 </div>
